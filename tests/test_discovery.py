9571c6bd3ac2dfb251a06a5e925a247980601f09
C0dEr8627 <umar.231405.it@mhssce.ac.in>
2026-09-18 01:14:01 +0530

    Add discovery coverage for embedded and encoded profile routes

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -23,3 +23,22 @@ def test_extracts_unique_profile_candidates():
 def test_ignores_nested_profile_paths():
     html = '<a href="/community/@alpha/posts">Ignore</a>'
     assert extract_candidates(html) == []
+
+
+def test_extracts_profile_routes_from_embedded_page_data():
+    html = r'''<script>
+      window.__DATA__ = {"profile":"/community/@embedded_one","other":"/community\\/@embedded_two"};
+    </script>'''
+
+    candidates = extract_candidates(html)
+
+    assert [candidate.alias for candidate in candidates] == ["embedded_one", "embedded_two"]
+
+
+def test_extracts_encoded_profile_route():
+    html = '<script>"url":"https://builder.aws.com/community/%40encoded_user"</script>'
+    candidates = extract_candidates(html)
+
+    assert candidates == [
+        type(candidates[0])("encoded_user", "https://builder.aws.com/community/@encoded_user")
+    ]
